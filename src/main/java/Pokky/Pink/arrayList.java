aab5f1c6f6d41fa3fbad69d99bf8be3a34a28d84
Ubolwan Vorakanonta <nuitabing@gmail.com>
2022-10-04 15:53:51 -0700

    add low to high

--- src/main/java/Pokky/Pink/arrayList.java
+++ src/main/java/Pokky/Pink/arrayList.java
@@ -9,21 +9,29 @@ public class arrayList {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Integer[] nums = {6, 1, 2, 3};
-		System.out.println(lowToHigh(nums) );
+		Integer[] nums = {6, 9, 16, 10, 0, 1, 2, 3};
+		System.out.println(lowToHigh(nums));
+		
 
 	}
 	
-	public static ArrayList lowToHigh(Integer[] abcd) {
-		List<Integer> nums = new ArrayList<>(Arrays.asList(abcd));
-		int temp;
+	public static List<Integer> lowToHigh(Integer[] abcd) {
+		List<Integer> numsOrig = new ArrayList<>(Arrays.asList(abcd));
+		List<Integer> numsNew = new ArrayList<>();
 		
-		for(int i = 0; i < nums.size(); i ++) {
-			if(abcd[i] < abcd[i+1]  ) {
-				temp = abcd[i];
-				nums.add(temp);
+		for(int i = 0 ; i < numsOrig.size() ; i++) {
+			int minVal = Integer.MAX_VALUE;
+			for(int j = 0; j < numsOrig.size(); j++) {
+				if(numsOrig.get(j) < minVal) {
+					if(numsNew.size() == 0 || numsNew.get(numsNew.size()-1) < numsOrig.get(j)) {
+						minVal = numsOrig.get(j);
+					}
+				}
 			}
+			numsNew.add(minVal);
 		}
+		
+		return numsNew;
 		
 	}
 	
